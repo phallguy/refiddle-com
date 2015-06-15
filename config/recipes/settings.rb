ae13906e6993af85decb257a711630c9f09d065b
Paul Alexander <paul@phallguy.com>
2015-06-15 11:50:21 -0700

    Update settings files

--- config/recipes/settings.rb
+++ config/recipes/settings.rb
@@ -2,6 +2,7 @@ set( :setting_prompts, {
   segmentio_secret: "Segment IO Secret: ",
   facebook_secret: "Facebook Secret: ",
   twitter_secret: "Twitter Secret: ",
+  google_secret: "Google Secret: ",
   aws_key: "AWS Key: ",
   aws_secret: "AWS Secret: ",
   stackexchange_secret: "StackExchange Secret: ",
